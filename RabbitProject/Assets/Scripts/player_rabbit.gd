5c743db990c2ca71c40ba5eca9df1144c218b913
Amrith <siminskimark@gmail.com>
2020-04-18 13:37:02 -0400

    Collisions-Fixed
    Hoorah!

--- RabbitProject/Assets/Scripts/player_rabbit.gd
+++ RabbitProject/Assets/Scripts/player_rabbit.gd
@@ -9,6 +9,7 @@ onready var tween = $Tween
 export var sped = 2
 #var playerloc = Vector2(30,2)
 onready var velo = Vector2(0, 0)
+onready var ray = $RayCast2D
 
 
 
@@ -26,10 +27,30 @@ func _process(delta):
 			move(dir)		
 
 func move_tween(dir):
-	var collision = move_and_slide(velo * 1)
-	if collision:
-		return
+	print(dir)
+	match dir:
+		Vector2(0,0):
+			pass
+		Vector2(1,0):
+			#RIGHT
+			ray.cast_to= Vector2(32,0)
+		Vector2(-1,0):
+			#LEFT
+			ray.cast_to=Vector2(-32,0)
+		Vector2(0,1):
+			#Down
+			ray.cast_to=Vector2(0,32)
+		Vector2(0,-1):
+			ray.cast_to=Vector2(0,-32)
+	ray.force_raycast_update()
+	var collision = move_and_collide(velo * 1)
+	if ray.is_colliding():
+		tween.interpolate_property(self, "position",
+			position, (position +(-2*ray.position)),
+			1.0/sped, Tween.TRANS_SINE, Tween.EASE_IN_OUT, 0)
+		tween.start()
 	else:
+		position = position +(dir)
 		tween.interpolate_property(self, "position",
 			position, (position + dir * tile_size),
 			1.0/sped, Tween.TRANS_SINE, Tween.EASE_IN_OUT, 0)
